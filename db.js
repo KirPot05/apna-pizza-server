b77f10bb1355c31c1ebab21347f2f30a779afa40
Kiran <kiran0503@gmail.com>
2022-06-07 11:44:53 +0530

    modified db credentials respective to docker container

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ import Sequelize from 'sequelize';
 
 const db = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'db',
-    port: 5425,
     dialect: 'postgres'
 });
 
